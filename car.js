90dfa807d530e04c077691075115f07b315b90e1
ellenshin314 <ellenshin314@gmail.com>
2025-06-28 12:17:07 -0700

    Refactor car control logic and neural network integration; update HTML canvas elements and styles; implement visualizer for neural network representation.

--- car.js
+++ car.js
@@ -6,17 +6,19 @@ class Car {
     this.height = height;
 
     this.speed = 0;
-    this.acceleration = 0.2;
+    this.acceleration = 0.1;
     this.maxSpeed = maxSpeed;
     this.friction = 0.05;
     this.angle = 0;
     this.damaged = false;
+    this.useBrain = controlType == "AI"
 
     if (controlType !== "dumdum") {
       this.sensor = new Sensor(this);
-      this.brain = new NeuralNetwork();
+      this.brain = new NeuralNetwork(
+        [this.sensor.rayCount, 6, 4]
+      );
     }
-    
     this.controls = new Controls(controlType);
   }
 
@@ -26,7 +28,20 @@ class Car {
       this.polygon = this.#createPolygon();
       this.damaged = this.#assessDamage(roadBorders, traffic);
     }
-    if (this.sensor) this.sensor.update(roadBorders, traffic);
+    if (this.sensor) {
+      this.sensor.update(roadBorders, traffic);
+      const offsets = this.sensor.readings.map((s) => {
+        s == null ? 0 : 1 - s.offsets;
+      });
+      const outputs = NeuralNetwork.feedForward(offsets, this.brain);
+
+      if (this.useBrain) {
+        this.controls.forward = outputs[0];
+        this.controls.left = outputs[1];
+        this.controls.right = outputs[2];
+        this.controls.reverse = outputs[3];
+      }
+    }
   }
 
   #assessDamage(roadBorders, traffic) {
--- confuse.js
+++ confuse.js
@@ -1,24 +1,95 @@
-let func = (what) => {
-  let that = what; //12
-  what += 14; //26
-  let you = what++; //27
-  this.then = what; //26
-  console.log(--what + that-- + that / this.then - 12); //25 + 12 + (11/26)
-  you += "hello"; //27 + "hello"
-  return 0; //0
-};
+// create me a class named human
 
-func(12);
-
-let falseTruths = () => {
-  var True = false;
-  var False = true;
-  var trues = 0;
-  if ((False || 1) == (False || 0)) {
-    trues = true + True + False + false + true;
+class Human {
+  constructor(brain, heart, height, speed) {
+    this.brain = brain;
+    this.heart = heart;
+    this.height = height;
+    this.speed = speed;
   }
-  console.log((trues + False && 0 + true) || (0 + False && 1));
-  return !(!(True && 1) == !!!(false || 0));
-};
 
-console.log(falseTruths());
+  printHeight() {
+    console.log(this.height);
+  }
+  sayWords(array) {
+    if (this.brain && this.heart) {
+      for (let i = array.length - 1; i > -1; i--) console.log(array[i]);
+    }
+  }
+}
+
+const ellen = new Human(true, true, "5'3", 2);
+const roy = new Human(false, true, "6'7", 75);
+const randy = new Human(true, false, "6'0", 10);
+
+const words1 = ["apple", "book", "car", "door", "earth"];
+const words2 = ["kite", "lamp", "moon", "net", "ocean"];
+const words3 = ["plant", "queen", "rock", "sun", "tree"];
+
+// ellen.sayWords(words1);
+// roy.sayWords(words2);
+// randy.sayWords(words3);
+
+//polymorphism
+class Animal {
+  constructor() {}
+  speak() {
+    console.log("animal sound");
+  }
+  eat() {
+    console.log("food");
+  }
+}
+
+class Dog extends Animal {
+  speak() {
+    console.log("bark");
+  }
+  eat() {
+    console.log("beef");
+  }
+}
+
+class Cat extends Animal {
+  speak() {
+    console.log("meow");
+  }
+  eat() {
+    console.log("fish");
+  }
+}
+
+class Fish extends Animal {
+  speak() {
+    console.log("blub");
+  }
+  eat() {
+    console.log("fish food");
+  }
+}
+
+class Bird extends Animal {
+  speak() {
+    console.log("tweet");
+  }
+  eat() {
+    console.log("seeds");
+  }
+}
+
+const labrador = new Dog();
+const goldFish = new Fish();
+const tabby = new Cat();
+const sparrow = new Bird();
+
+labrador.speak();
+tabby.speak();
+goldFish.speak();
+sparrow.speak();
+
+console.log("");
+
+labrador.eat();
+tabby.eat();
+goldFish.eat();
+sparrow.eat();
